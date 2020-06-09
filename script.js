21c3f72cfefad8062484c568b93f67110306718f
igorujevic <iujevi00@fesb.hr>
2020-06-09 17:47:14 +0200

    audio for app, play pause btn

--- script.js
+++ script.js
@@ -22,3 +22,35 @@ function breathAnimation() {
 }
 
 setInterval(breathAnimation, totalTime);
+
+// functions for play / stop music, and display btn text
+function playStopMusic() {
+  var player = document.getElementById('player');
+
+  player.paused == true ? toggle(0) : toggle(1);
+}
+
+function toggle(state) {
+  var player = document.getElementById('player');
+  var btnText = document.getElementById('btn-audioPlay');
+  var src = '/audio/relaxsound.mp3';
+
+  switch (state) {
+    case 0:
+      player.src = src;
+      player.load();
+      player.play();
+      btnText.innerHTML = 'Pause Music';
+      btnText.className = 'btn btn-pause';
+      player_state = 1;
+      break;
+    case 1:
+      player.pause();
+      player.currentTime = 0;
+      player.src = '';
+      btnText.innerHTML = 'Play Music';
+      btnText.className = 'btn btn-play';
+      player_state = 0;
+      break;
+  }
+}
